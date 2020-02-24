cdc2bca947c0677036b080e3814b1256448a004c
Pavlov Dev <pavlov-dev@vas.cis.valpo.edu>
2020-02-24 16:31:27 -0600

    User Edit, Full Name, Search functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,22 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-	 
-
-  def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid
-      user.email = auth.info.email
-      user.name = auth.info.name
-      user.first_name = auth.info.first_name
-      user.last_name = auth.info.last_name
-      #user.image = auth.info.image
-      user.password = Devise.friendly_token[0,20]
-      #user.access_token = auth.credentials.token
-      #user.refresh_token = auth.credentials.refresh_token
-      #user.oauth_expires_at = Time.at(auth.credentials.expires_at)
-      user.save!
-    end
-  end
+  
 end
